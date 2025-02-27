a86b8b1176ce9014600f33608cc4e2d91b119abb
Ontonieja <ontonieja@gmail.com>
2025-02-27 12:34:09 +0100

    create color variables and change defautl styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,20 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "background": "var(--background)",
+        "foreground": "var(--foreground)",
+        "primary-orange": "#ECC054",
+        "primary-background": "#EFEFEF",
+        "primary-button": "# 16132B",
+        "light-blue": "#8AB7DF",
+        "muted-gray": "#16132B",
       },
+      fontFamily: {
+        grotesk: "var(--font-space-grotesk)",
+      },
+    },
+    maxWidth: {
+      "1920": "1920px",
     },
   },
   plugins: [],
