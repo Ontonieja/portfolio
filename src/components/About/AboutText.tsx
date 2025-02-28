d503cedc955592a5f79e6a6dc20affc71e5581fd
Ontonieja <ontonieja@gmail.com>
2025-02-28 09:58:31 +0100

    create panel with about text and lottie animation

---
+++ src/components/About/AboutText.tsx
@@ -0,0 +1,24 @@
+export default function AboutText() {
+  return (
+    <div className="flex flex-col space-y-6 mt-6">
+      <p>
+        I&apos;m Max, a full-stack developer skilled in{" "}
+        <b>Next.js, Node.js, and React</b>, building both frontend and backend
+        solutions. I create efficient, scalable applications with clean code and
+        a strong focus on performance.
+      </p>
+      <p>
+        I&apos;ve developed projects for various industries, including a website
+        for a{" "}
+        <b>paraboxing organization and a platform for a cat breeding company</b>
+        . Whether it&apos;s crafting seamless user experiences or architecting
+        powerful backend systems, I bring technical expertise and
+        problem-solving skills to every project.
+      </p>
+      <p>
+        I&apos;m always open to new challenges—
+        <b>let`s build something great together!</b> 🚀
+      </p>
+    </div>
+  );
+}
