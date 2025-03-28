5cb902e23de5a769ee2951374b1f2ed34e9ad1e0
Ontonieja <ontonieja@gmail.com>
2025-03-28 09:41:05 +0100

    adjust navbar to project page

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -5,9 +5,18 @@ import NavLinksWithIntersection from "./NavLinksWithIntersection";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import MobileNav from "./MobileNav";
+import { usePathname } from "next/navigation";
+import SocialLink from "../ui/SocialLink";
+import { FaGithubAlt } from "react-icons/fa6";
+import { IoLogoLinkedin } from "react-icons/io5";
 
-export default function Navbar() {
+export default function Navbar({
+  backgroundColor,
+}: {
+  backgroundColor?: string;
+}) {
   const [scrolled, setScrolled] = useState(false);
+  const isProjectPage = usePathname().startsWith("/projects");
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,19 +33,45 @@ export default function Navbar() {
   return (
     <header
       className={cn(
-        "left-0 text-sm w-full py-5 sticky px-6 xl:px-12 sm:mt-3 transition-all duration-300 top-0 z-50 bg-white",
-        scrolled ? "bg-white shadow-xl" : "bg-transparent"
+        "left-0 text-sm w-full py-5 sticky px-4 xl:px-12 transition-all duration-300 top-0 z-50 bg-white",
+        scrolled ? "bg-white shadow-xl" : "bg-transparent",
+        isProjectPage && "text-white",
+        backgroundColor && backgroundColor
       )}
     >
-      <nav className="flex justify-between items-center w-full max-w-1920 mx-auto">
+      <nav
+        className={cn(
+          "flex justify-between items-center w-full  mx-auto sm:pt-3",
+          isProjectPage ? "max-w-[1512px]" : "max-w-[1920px]"
+        )}
+      >
         <div className="relative bg-transparent">
           <Link className="font-bold ml-2 z-50" href={"/"}>
             Maksymilian Rusnak
           </Link>
-          <div className="absolute w-9 h-9 -z-10 bg-primary-orange rounded-full top-1/2 left-0 transform  -translate-y-1/2"></div>
+          <div className="absolute w-9 h-9 -z-10  rounded-full top-1/2 left-0 transform bg-primary-orange -translate-y-1/2"></div>
         </div>
         <div className="hidden sm:flex items-center gap-5">
-          <NavLinksWithIntersection />
+          {!isProjectPage ? (
+            <NavLinksWithIntersection />
+          ) : (
+            <div className="flex gap-4">
+              <>
+                <SocialLink
+                  href="https://github.com/Ontonieja"
+                  Icon={FaGithubAlt}
+                  text="Github"
+                  isProjectPage={isProjectPage}
+                />
+                <SocialLink
+                  href="https://www.linkedin.com/in/maksymilian-rusnak/"
+                  Icon={IoLogoLinkedin}
+                  text="Linkedin"
+                  isProjectPage={isProjectPage}
+                />
+              </>
+            </div>
+          )}
         </div>
         <MobileNav />
       </nav>
