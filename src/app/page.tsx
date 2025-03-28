66dbbf325e16fc8b9592e77d73d491b5dc9b5179
Ontonieja <ontonieja@gmail.com>
2025-03-28 09:30:47 +0100

    add navbar to home page and create new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
-import AboutPanel from "@/components/About/AboutPanel";
-import HeroPanel from "@/components/Hero/HeroPanel";
+import AboutPanel from "@/components/about/AboutPanel";
+import Navbar from "@/components/header/Navbar";
+import HeroPanel from "@/components/hero/HeroPanel";
+import PageTransition from "@/components/PageTransition";
+import PortfolioPanel from "@/components/portfolio/PortfolioPanel";
 import SectionWrapper from "@/components/SectionWrappers";
 
 export default function Home() {
   return (
-    <main className="flex flex-col space-y-6 md:space-y-12 px-6 xl:px-12">
-      <SectionWrapper id="/" className="md:mt-2">
-        <HeroPanel />
-      </SectionWrapper>
-      <SectionWrapper id="about">
-        <AboutPanel />
-      </SectionWrapper>
-    </main>
+    <>
+      <Navbar />
+      <main className="flex flex-col space-y-6 md:space-y-8 lg:space-y-12 px-4 xl:px-12">
+        <PageTransition>
+          <SectionWrapper id="/" className="md:mt-2">
+            <HeroPanel />
+          </SectionWrapper>
+          <SectionWrapper id="about">
+            <AboutPanel />
+          </SectionWrapper>
+
+          <SectionWrapper id="projects">
+            <PortfolioPanel />
+          </SectionWrapper>
+        </PageTransition>
+      </main>
+    </>
   );
 }
